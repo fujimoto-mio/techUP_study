a2d92e753d9f9a892bb5c11a85088175500914ea
yuutatakeda <scream.tkyuta@gmail.com>
2022-07-25 20:53:17 +0900

    課題3-5

---
+++ 武田侑大/3_5/js/script.js
@@ -0,0 +1,45 @@
+
+console.log( '1' + 2 );
+console.log( '1' + '2' );
+console.log( "1" + "2" );
+console.log( 2 + '1' );
+console.log( 2 + 2 + '1' );
+console.log( 6 - '2' );
+console.log( 1 + 2 );
+console.log( 6 - "2" );
+
+
+let a, b, c;
+a = b = c = 2 + 2;
+console.log( a );
+console.log( b );
+console.log( c );
+
+let n = 2;
+n = n + 5;
+console.log(n);
+n = n * 2;
+console.log(n);
+
+var f = 2;
+f += 5;
+console.log(f);
+f *= 2;
+console.log(f);
+
+
+
+ let counter = 2;
+ counter++;
+ console.log( counter );
+
+ let counter2 = 2;
+ ++counter2;
+ console.log( counter2 );
+
+ let counter3 = 2;
+ counter3--;
+ console.log( counter3 );
+ let counter4 = 2;
+ --counter4;
+ console.log( counter4 ); 
