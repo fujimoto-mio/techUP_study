851f16bb7d196321b5576dc77b7811b167ca0dc5
yuutatakeda <scream.tkyuta@gmail.com>
2022-08-01 08:55:48 +0900

    課題3-10

---
+++ 武田侑大/3-10/js/script.js
@@ -0,0 +1,8 @@
+const names = ["山田さん","佐藤さん","内田さん"];
+console.log(names);
+const shiftExamle1  = names.shift();
+  console.log('山田さんを消しました。: ' + names);
+  const shiftExamle2  = names.shift();
+    console.log('佐藤さんを消しました。: ' + names);
+    const shiftExamle3  = names.shift();
+      console.log('データがなくなりました。');
