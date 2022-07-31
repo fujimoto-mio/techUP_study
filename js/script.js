b5e6867896b1b17d64c67755bb1ccdc52c25400f
fujimoto <fujimoto04@gmail.com>
2022-07-31 23:38:50 +0900

    master init

--- js/script.js
+++
@@ -1,1 +0,0 @@
-// Write JavaScript here 
--- 松岡裕次郎/3-4/js/script.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-    let text1;
-    text1 = 'こんにちは'; 
-    console.log(text1); 
-    let age1="28", user1="松岡裕次郎";
-    console.log(age1);
-    console.log(user1);
