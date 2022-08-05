5985ab307e6fe842916039e6f03a8238b4a520c1
yuutatakeda <109027218+yuutatakeda@users.noreply.github.com>
2022-08-05 22:25:04 +0900

    Revert "武田課題3-11 mapでoutputを出力"

--- 武田侑大/3-11/js/script.js
+++ 武田侑大/3-11/js/script.js
@@ -1,13 +1,7 @@
-const objects = [
-  {input:["a","b","c"]},
-{output:[{0: "a"}, {1: "b"}, {2: "c"}]}
-]
 
-const object = objects.map( obj =>{
-
-  return [ obj.output ]
-});
-
-
+const object = {
+  input: [“a”,”b”,”c”],
+  output: [{0: “a”}, {1: “b”}, {2: “c”}]
+};
 
 console.log(object);
