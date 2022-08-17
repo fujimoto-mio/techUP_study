13f1873aa6b10885a1115552f5dce721fa52ddd3
yujiro <macchan0827.x@gmail.com>
2022-08-17 22:25:05 +0900

    松岡/3-10課題/要素の変更

---
+++ 松岡裕次郎/3-10/script.js
@@ -0,0 +1,11 @@
+const names = ['山田さん', '佐藤さん', '内田さん',];
+  console.log('出力結果: ' + names);
+
+  names.splice(0,1,);
+  console.log('出力結果: ' + names);
+
+  names.splice(0,1,);
+  console.log('出力結果: ' + names);
+
+  names.splice(0,1,'no date');
+  console.log('出力結果: ' + names);
