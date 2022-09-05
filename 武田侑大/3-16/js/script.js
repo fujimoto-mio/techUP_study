5dfc9701ef9f38ee2ecd167e1cb46c621935b9c4
yuutatakeda <scream.tkyuta@gmail.com>
2022-09-05 20:30:33 +0900

    3-16確認

--- 武田侑大/3-16/js/script.js
+++ 武田侑大/3-16/js/script.js
@@ -1,16 +1,26 @@
-const btn1 = document.getElementsByClassName("change");
-const btn2 = document.getElementsByClassName('reset');
+const btn1 = document.getElementById("change");
+
 const item = document.getElementsByClassName('list-group-item');
 
-console.log(item)
 
-for (let i = 0; i < item.length ; i++) {
-    (function(n) {
-         btn1[n].addEventListener('click', () => {
-        item = n * 10
+   btn1.addEventListener("click",function(){
+
+    console.log(item);
+})
+
+
+
+
+
+
+
+//for (let i = 0; i < item.length ; i++) {
+  //  (function(n) {
+   //      btn1[n].addEventListener('click', () => {
+    //        item.innerText = n * 10
      
-    })
+   // })
 
-});
+//});
 
-}
+//}
