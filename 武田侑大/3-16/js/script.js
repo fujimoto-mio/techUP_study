ea58c051941f37c79babed81bc70925436582d6c
yuutatakeda <scream.tkyuta@gmail.com>
2022-09-06 18:26:22 +0900

    武田3-16変更ボタン

--- 武田侑大/3-16/js/script.js
+++ 武田侑大/3-16/js/script.js
@@ -1,27 +1,16 @@
-const btn1 = document.getElementById("change");
-const output = document.getElementById("output");
-
-console.log(output.textContent);
-
-
-
-   btn1.addEventListener("click",function(){
-   let test = output.textContent
-    output.textContent = test * 2
-
-});
-
-///////////////////////////////////////////////////
+let btn1 = document.getElementById("change");
+let output = document.getElementById("output");
 
 let output2 = document.getElementsByClassName("list-group")
 let li = document.getElementsByClassName("list-group-item")
 
-console.log(li[0].textContent);
 
 for (let i = 0; i < li.length ; i++) {
+  
    btn1.addEventListener("click",function(){
-   let test2 = li[0].textContent
-    li.textContent = test2 * 10
+    
+      let test2 = li[i].textContent
+      li[i].textContent = test2 * 10
 
 });
 
