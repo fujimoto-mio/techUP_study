978e667c359d134469fc41bbe4cc2d92fcc7c1eb
yuutatakeda <scream.tkyuta@gmail.com>
2022-09-11 00:02:18 +0900

    課題３－１６武田DOM

--- 武田侑大/3-16/js/script.js
+++ 武田侑大/3-16/js/script.js
@@ -1,17 +1,27 @@
 let btn1 = document.getElementById("change");
-let output = document.getElementById("output");
-
-let output2 = document.getElementsByClassName("list-group")
-let li = document.getElementsByClassName("list-group-item")
+let btn2 = document.getElementById("reset");
+let li = document.getElementsByClassName("list-group-item");
 
 
-for (let i = 0; i < li.length ; i++) {
-  
+
    btn1.addEventListener("click",function(){
-    
-      let test2 = li[i].textContent
-      li[i].textContent = test2 * 10
-
+      for (let i = 0; i < li.length ; i++) { 
+            let test = li[i].textContent ;
+            li[i].textContent = test * 10 ;
+            
+            }
 });
 
-}
+    
+    
+    btn2.addEventListener("click",function(){
+     
+      for (let i = 0; i < li.length ; i++) { 
+            
+           li[0].textContent = 100;
+           li[1].textContent = 110;
+           li[2].textContent = 120;
+           li[3].textContent = 130;
+           li[4].textContent = 140;
+            }
+});
