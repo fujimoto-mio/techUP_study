97a72defe0967548986b9e37445d089ae6fa6a2b
otsukiryoga <robinandboo@gmail.com>
2022-11-15 20:31:49 +0900

    大月 課題6_2　改行修正

--- 大月凌雅/Level6-2/index.php
+++ 大月凌雅/Level6-2/index.php
@@ -1,21 +1,21 @@
 <?php 
 
 	$now = time() ;
-	echo date("Y/m/d H:i:s \n", $now) ;
+	echo "今は".date("Y/m/d H:i:s", $now)."です。</br>" ;
 
 	$yesterday = strtotime("-1 day") ;
-	echo "昨日は".date("Y/m/d", $yesterday)."です。\n" ;
+	echo "昨日は".date("Y/m/d", $yesterday)."です。</br>" ;
 
 	$tomorrow = strtotime("+1 day") ;
-	echo "明日は".date("Y/m/d", $tomorrow)."です。\n" ;
+	echo "明日は".date("Y/m/d", $tomorrow)."です。</br>" ;
 
 	$nextweek = strtotime("+1 week") ;
-	echo "来週は".date("Y/m/d", $nextweek)."です。\n" ;
+	echo "来週は".date("Y/m/d", $nextweek)."です。</br>" ;
 	
 
 	$three_month = strtotime("+3 month") ;
 	$date_threemonth = date("Y-m-d",$three_month);
-	echo "3カ月後は".date("Y/m/d", $three_month)."です。\n" ;
+	echo "3カ月後は".date("Y/m/d", $three_month)."です。</br>" ;
 
 	date_default_timezone_set('Asia/Tokyo');
 
