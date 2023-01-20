a153a65229f84c0d731c8b583187f2c7ae5d80f1
19yuki99 <shinmura.yuuki0105@gmail.com>
2023-01-20 10:16:23 +0900

    Level8-2

--- Sample/app/Console/Kernel.php
+++ Sample/app/Console/Kernel.php
@@ -2,11 +2,20 @@
 
 namespace App\Console;
 
+use App\Console\Commands\BirthdayMail;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    /**
+     * アプリケーションで提供するArtisanコマンド
+     *
+     * @var array
+     */
+    protected $commands = [
+        BirthdayMail::class
+    ];
     /**
      * Define the application's command schedule.
      *
@@ -16,6 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        // テストように毎分に実行します
+        $schedule->command('send:birthday-mail')->cron('* * * * *');
     }
 
     /**
