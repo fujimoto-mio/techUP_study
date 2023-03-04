65f39611df41f0f711492095cdace5222d3cf836
Ohno_Naoki <naoki19990729@gmail.com>
2023-03-04 21:17:39 +0900

    課題提出

---
+++ 大野尚貴/Level6-4/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>index.php</title>
+</head>
+<body>
+<form method="post" action="index.php">
+名前<input type="text" name="name">
+<input type="button" name="btn1" valte="送信">
+</form>
+
+<?php
+  setcookie("user", "name", time()-60);
+  $var($COOKIE["name"]);
+
+  try{
+    if(isset($POST["name"])){
+        if($POST["name"] == ""){
+            throw new Exception("入力してからボタンを押してください。<br>");
+            }        
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+         }finally{ 
+            echo "処理が終了しました";
+            }
+            
+            session_set_cookie_params(10);
+          session_start();
+          if (!isset($SESSION['count'])) {
+              $_SESSION['count'] = 1;
+          } else {
+              $_SESSION['count']++;
+          }
+          echo $_SESSION['count']."回目の訪問です。";              
+          ?> 
+</body>
+</html>
