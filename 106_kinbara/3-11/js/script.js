7f246af96ab71b7441602ced19096757243889e5
金原えみ <emi03mail@gmail.com>
2023-03-08 23:59:49 +0900

    金原/3-11/構文の使い方

---
+++ 106_kinbara/3-11/js/script.js
@@ -0,0 +1,11 @@
+var map = new Map();
+  
+map.set('a', 'a_set');
+map.set('b', 'b_set');
+map.set('c', 'c_set');
+
+console.log(map.get('a'));
+console.log(map.get('b'));
+console.log(map.get('c'));
+
+
