c4468625dd717cebcba0c99945f80f46a035ed60
Serika-Yanagi <serika.11268@gmail.com>
2023-03-22 22:02:01 +0900

    柳3-13.15再提出

--- 61_yanagi_serika/Kansei3-8~15/3-13/script.js
+++ 61_yanagi_serika/Kansei3-8~15/3-13/script.js
@@ -1,13 +1,11 @@
-//要素取得
+
 const saikoro = document.getElementById("saikoro");
 
-//サイの目
 function sainome(){
 const nmb = Math.floor(Math.random()*6 + 1);
 saikoro.innerHTML = nmb; 
 }
 
-//クリックイベント
 
 
 document.addEventListener('click', sainome, false);
