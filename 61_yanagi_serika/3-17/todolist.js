eab30d445c6086ba50dc44fd50cbedc02432857c
Serika-Yanagi <serika.11268@gmail.com>
2023-04-29 18:23:50 +0900

    consoleから脱出！表示完了

--- 61_yanagi_serika/3-17/todolist.js
+++ 61_yanagi_serika/3-17/todolist.js
@@ -1,7 +1,7 @@
 // グローバルの空間に変数や関数をセットしている。（即時関数にしてもよい）
 function todo() {
     // 入力したTodoタスクの一覧を保持する配列を定義する
-    const todos =[];
+    const todo_array =[];
    
     /* HTMLのID値を使ってDOM要素を取得する */
     //  1 テキストボックス(input[type="text"])を要素を取得する。ここでは、[input-todo-box]
@@ -11,62 +11,66 @@ function todo() {
     //  3 Todoリストを一覧表示するul要素を取得する。ここでは、[todo-list]
         //addTaskTargetが[todo-list]
   
-    const inputBox = document.getElementById('input-todo-box');
-    const addButton = document.getElementById('add-button');
-    const addTaskTarget = document.getElementById('todo-list');
+    const inputBox = document.getElementById('input-todo-box');//入力文字
+    const addButton = document.getElementById('add-button');//追加
+    const addTaskTarget = document.getElementById('todo-list');//表示
 
 
     /*「追加」ボタンがクリックされたときの処理 */
     //addButtonが[add-buton]追加
     addButton.addEventListener('click', (event) => {
+      let count = event.detail;
+      console.log('Click count is ' + count);
+
       //   テキストボックス(input-todo-box)[inputBox]に入力された(テキストの値を取り出して、Todoリスト一覧に追加する)
       const todos = { comment: inputBox.value,  };
+      //todosは入力した文字とつながりあり、、
       //   取り出したらテキストボックスの中を空にする
       inputBox.focus();
-   
-      //level3-10 [配列の要素を変更しよう]push()要素を参考に配列でテキストの値を追加する。????????????????
-      //同じ？
-      var todo_array = [todos,todos,todos];
-      console.log(todos);   //これで入力した文字がconsole内表示される
-      todo_array.push();
-      console.log(todo_array+'deletTodo');
- 
-      //↑todoリスト一覧の配列 {comment: inputBox.value,}はtodoで紐付けているから紐付けた[todo]のarray
-
+      //level3-10 [配列の要素を変更しよう]push()要素を参考に配列でテキストの値を追加する。
+      //これで入力した文字がconsole内表示される
+      todo_array.push(todos);
+      console.log(todo_array);
       //showTodos()関数で再表示
       showTodos();   //← これでリスト内に表示される？
    
     });
 
-    
-//上記34のshowTodos()の説明
 /* 「todos」の中身を一覧表示する */
 function showTodos() {
-  //    - ul要素にli要素(createElement)を追加して、li要素内にtodoタスクの内容を表示する
-     //addTaskTargetの中のli
-  const tableId = document.createElement('addTaskTarget');//（）内に''を入れないとエラーになる、、、、
-  console.log(tableId);//addTaskTarget<><>表示される
-  tableId.id = todos;
-  tableId.innerHTML=todos;
+  //    - ul要素にli要素(createElement)エリアを追加して、li要素内にtodoタスクの内容を表示する
+  //addTaskTargetの中のli
+  const tableId = document.createElement('addTaskTarget');
 
-  console.log(todos);
-  //TodoリストをforEach繰り返しにして作成する
-  todos.forEach((todo, index) => {
+ todo_array.forEach((todo, index) => {
+  //--------------
+    var inputBox = document.getElementById('input-todo-box');
+    var todos = { comment: inputBox.value,  };
 
-    //appendChildを使ってノードを追加する
-    todos.appendChild(tableId);
-    //リストのli要素内に削除ボタン（deleteTodo）をつける（document.createElement('button');）
+    //-----------------
+    //todo[index]の大きい数を取得したい！「大きい=length」
+    var todo_length = todo_array.length;
+    //一つずつ大きい数でその中のテキスト
+    for (var i = 0; i < todo_length; i++) {
+      todo_array[i].textContent=todo_array[i].textContent
+
+     };
+    
+    var text = document.createTextNode(todo_length);//生成するテキストノードに含めるテキストを指定する。
+    //表示させる内容
+    tableId.appendChild(text);//tableId内は文字が入る
+    addTaskTarget.appendChild(tableId);//tableIdはaddTaskTarget内にあり
+
     var deleteTodo = document.createElement('button');  
-    deleteTodo.lastElementChild.remove();
     deleteTodo.textContent = '削除';
     //削除（deleteTodo）をクリックされた時、showTodos()関数で削除する
-    document.getElementById("button").onclick = function() {
-      showTodos();
-    };
+    deleteTodo.addEventListener('click', () => {
+      todos.remove();
+    });
   });
 }
 
-
+console.log(deleteTodo);
 /* todoの内容を削除する */
 // Todo情報を表すli要素(showTodo関数で作成される要素)の中にある削除ボタン[deleteTodo]をクリックしたら実行される。
 function deleteTodo(index) {
