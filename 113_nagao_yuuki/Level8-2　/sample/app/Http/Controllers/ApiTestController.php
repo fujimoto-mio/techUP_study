d79aef5b66108d50fb9985dce6e0902fa0ec2ce0
yuki_nagao <yuki19930108@outlook.jp>
2023-05-18 17:01:18 +0900

    途中経過

---
+++ 113_nagao_yuuki/Level8-2　/sample/app/Http/Controllers/ApiTestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiTestController extends Controller
+{
+    //
+    //ApiTestControllerに　dataInsert関数を追加して、その関数に挿入プログラムを作成してください。
+}
