362d6e825dd02b3ea7d492e8d874c7f67ec0e162
toyoshima <army.v0610@gmail.com>
2023-05-29 23:12:37 +0900

    インデント修正

--- 89_toyoshima_erina/Laravel/Sample/app/Http/Controllers/ApiSample.php
+++ 89_toyoshima_erina/Laravel/Sample/app/Http/Controllers/ApiSample.php
@@ -6,8 +6,10 @@ use Illuminate\Http\Request;
 
 class ApiSample extends Controller
 {
-    public function apiHello(){
-        return response()->json(
+    public function apiHello()
+    {
+        return response()->json
+        (
             [
                 'name' => '田中',
                 'nick_name' => 'tanaka',
@@ -15,8 +17,10 @@ class ApiSample extends Controller
                 "profile" => ["sport" => 'basebool', "like" => "move"]
             ]
         );
-} 
-    public function nameSet(Request $request) {
+    }
+
+    public function nameSet(Request $request) 
+    {
         $member = new Task;
         $member->name = $request->name;
         $member->status = 1;
