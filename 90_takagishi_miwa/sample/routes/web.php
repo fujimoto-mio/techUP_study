6799b8242c3ffea1fa091b206f7b12ff7400a5a0
miwa <muyezhenyimei@gmail.com>
2023-06-21 21:29:33 +0900

    P-9/髙岸/Laravelフレームワーク（実装編）

--- 90_takagishi_miwa/sample/routes/web.php
+++ 90_takagishi_miwa/sample/routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/list');
 });
+
+Route::get('/list', [\App\Http\Controllers\TodoListController::class, 'index']);
+
+ 
+//Route::get( アドレス , [コントローラーの名前::class , メソッド名] );
+
+
+    
+
+    
+
+
+
