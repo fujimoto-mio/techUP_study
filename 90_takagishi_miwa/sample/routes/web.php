9ae9dc6dff741ec3e0a407f586e0a1535190271c
miwa <muyezhenyimei@gmail.com>
2023-06-24 11:11:21 +0900

    P-10/髙岸/PDOオブジェクトを使う

--- 90_takagishi_miwa/sample/routes/web.php
+++ 90_takagishi_miwa/sample/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TaskController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/list');
 });
+
+Route::get('/list', [\App\Http\Controllers\TodoListController::class, 'index']);
+
+Route::get('/',[TaskController::class,'index']);  //一覧表示用のURLとコントローラ場所
+Route::post('/create',[TaskController::class,'create']);  //タスク追加用のURLとコントローラ場所
+Route::post('/edit',[TaskController::class,'edit']);  //タスク更新用
+Route::post('/delete',[TaskController::class,'delete']);  //タスク削除用
+//Route::get( アドレス , [コントローラーの名前::class , メソッド名] );
+
+Route::resource('tasks', TaskController::class); 
+    
+
+    
+
+
+
