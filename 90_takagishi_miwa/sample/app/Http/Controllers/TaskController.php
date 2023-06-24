cafa0b0e00a1a2ddc9aa4dbb57d863d47c21fcbe
miwa <muyezhenyimei@gmail.com>
2023-06-24 11:11:21 +0900

    P-10/髙岸/PDOオブジェクトを使う

---
+++ 90_takagishi_miwa/sample/app/Http/Controllers/TaskController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+use Illuminate\Support\Facades\Validator;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('tasks.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $rules = [
+            'task_name' => 'required|max:100',
+          ];
+          //エラー時のメッセージ
+          $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+         
+        　//バリデーションのエラーならここで処理をします。
+        Validator::make($request->all(), $rules, $messages)->validate();
+         
+        $task = new Task;
+
+        $task->name = $request->input('task_name');
+        // ※関数内容はこちらにあります。→vendor/symfony/var-dumper/Resources/functions/dump.php
+        $task->save();
+
+        return redirect('/tasks');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
