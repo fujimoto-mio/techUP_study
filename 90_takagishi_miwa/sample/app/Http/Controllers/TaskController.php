8c6b109a964f5b3322721de91570d2bf14807ab4
miwa <muyezhenyimei@gmail.com>
2023-06-28 20:56:56 +0900

    P-10/髙岸/PDOオブジェクトを使う

--- 90_takagishi_miwa/sample/app/Http/Controllers/TaskController.php
+++ 90_takagishi_miwa/sample/app/Http/Controllers/TaskController.php
@@ -15,7 +15,10 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return view('tasks.index');
+        $tasks = Task::where('status', false)->get();
+        //dd($tasks);
+        // 'tasks'=> $tasks 引数で渡している。
+        return view('tasks.index', ['tasks'=> $tasks]);
     }
 
     /**
@@ -31,7 +34,7 @@ class TaskController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -39,13 +42,13 @@ class TaskController extends Controller
         $rules = [
             'task_name' => 'required|max:100',
           ];
-          //エラー時のメッセージ
-          $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
-         
-        　//バリデーションのエラーならここで処理をします。
+        //エラー時のメッセージ
+        $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+        
+        //バリデーションのエラーならここで処理をします。
         Validator::make($request->all(), $rules, $messages)->validate();
          
-        $task = new Task;
+        $task = new Task();
 
         $task->name = $request->input('task_name');
         // ※関数内容はこちらにあります。→vendor/symfony/var-dumper/Resources/functions/dump.php
@@ -64,7 +67,7 @@ class TaskController extends Controller
     {
         //
     }
-
+    
     /**
      * Show the form for editing the specified resource.
      *
@@ -73,7 +76,11 @@ class TaskController extends Controller
      */
     public function edit($id)
     {
-        //
+        // idから$taskを取得
+        $task = Task::find($id);
+        //dd($task);//何がでているか確認しよう。
+        //var_dump($task);//何がでているか確認しよう。
+        return view('tasks.edit', ['task'=> $task]);
     }
 
     /**
@@ -85,8 +92,48 @@ class TaskController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        if ($request->status === null) {
+ 
+            //バリデーションです
+            $rules = [
+                'task_name' => 'required|max:100',
+            ];
+ 
+            $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+ 
+            Validator::make($request->all(), $rules, $messages)->validate();
+ 
+ 
+            //該当のタスクを検索　取得する
+            $task = Task::find($id);
+ 
+            //モデル->カラム名 = 値 で、データnamwを割り当てる（更新する）
+            $task->name = $request->input('task_name');
+ 
+            //データベースに保存
+            $task->save();
+        }else {
+            //「完了」ボタンを押したとき
+ 
+            //該当のタスクを検索
+            $task = Task::find($id);
+ 
+            // statusを　trueにして完了に変えます。
+            //モデル->カラム名 = 値 で、データを割り当てる
+            $task->status = true; //true:完了、false:未完了
+ 
+            //データベースに保存
+            $task->save();
+        }
+ 
+        //リダイレクト
+        return redirect('/tasks');
     }
+ 
+
+            
+    
+    
 
     /**
      * Remove the specified resource from storage.
@@ -96,6 +143,8 @@ class TaskController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Task::find($id)->delete();
+
+        return redirect('/tasks');
     }
 }
