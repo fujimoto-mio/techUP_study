57adb5fbe38976f1b968076da145e90489ab15bb
金原えみ <emi03mail@gmail.com>
2023-06-29 22:44:35 +0900

    金原/6-2/PHPを書いていこう！

--- 106_kinbara/6-2/6-2.php
+++ 106_kinbara/6-2/6-2.php
@@ -6,7 +6,7 @@
 $d=date("Y-m-d H:i:s");
 print "課題１\n今日の日付と時間";
 print "<br>";
-print "$d";
+print $d;
 print "<br>";
 
 //課題2
@@ -14,21 +14,21 @@ print "<br>";
 $yesterday=date("Y-m-d",strtotime("-1 day"));
 print "課題2-1\n昨日";
 print "<br>";
-print "$yesterday";
+print $yesterday;
 print "<br>";
 
 //明日
 $tomorrow=date("Y-m-d",strtotime("1 day"));
 print "課題2-2\n明日";
 print "<br>";
-print "$tomorrow";
+print $tomorrow;
 print "<br>";
 
 //一週間後
 $nextweek=date("Y-m-d",strtotime("1 week"));
 print "課題2-3\n1週間後";
 print "<br>";
-print "$nextweek";
+print $nextweek;
 print "<br>";
 
 
@@ -37,7 +37,7 @@ print "<br>";
 $threeMonthLater=date("Y-m-d",strtotime("3 month"));
 print "課題3-1\n３か月後";
 print "<br>";
-print "$threeMonthLater";
+print $threeMonthLater;
 print "<br>";
 
 //今日から何日後
@@ -48,7 +48,7 @@ $days = $diff->days;
 
 print "課題3-2\n３か月後は何日後？";
 print "<br>";
-print "$days";
+print $days;
 
 ?>
  
