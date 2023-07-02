711832b1ea3db38ca7dc8006e4ac55b15c56925a
miwa <muyezhenyimei@gmail.com>
2023-07-02 13:44:45 +0900

    Level8-1/髙岸/API

---
+++ 90_takagishi_miwa/sample/app/Http/Controllers/ApiSample.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiSample extends Controller
+{
+        public function apiHello(){
+        return 'Hello API!!';
+    }
+}
