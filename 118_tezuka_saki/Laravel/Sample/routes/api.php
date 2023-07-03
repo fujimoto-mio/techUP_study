3741d3628db56bca33dc7cfa781359c3398d54bc
s-tezuka <tdksk.thunder@gmail.com>
2023-07-03 09:44:03 +0900

    L8-1提出

--- 118_tezuka_saki/Laravel/Sample/routes/api.php
+++ 118_tezuka_saki/Laravel/Sample/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiSample;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/apisample', [ApiSample::class, 'apiHello']);
+Route::post('/nameset', [ApiSample::class, 'nameSet']);
