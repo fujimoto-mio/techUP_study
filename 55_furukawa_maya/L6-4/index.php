faa750863351cd0aaf74d678c9fd75db96b3f9a8
FURUKAWAMAYA <myy.f310@gmail.com>
2023-09-15 15:56:48 +0900

    L6-4課題提出

---
+++ 55_furukawa_maya/L6-4/index.php
@@ -0,0 +1,47 @@
+<?php
+// セッション管理開始
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    try {
+        // フォームから送信された名前を取得
+        $name = $_POST['name'];
+
+        // 名前が空であればエラーを表示
+        if (empty($name)) {
+            throw new Exception("エラーです");
+        }
+
+        // 名前をCookieに保存（有効期限は10秒）
+        setcookie("user", $name, time() + 10);
+
+        // セッションの訪問回数を算出
+        if (!isset($_SESSION['count'])) {
+            $_SESSION['count'] = 1;
+        } else {
+            $_SESSION['count']++;
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+
+// クッキーから名前を取得
+$nameCookie = isset($_COOKIE["user"]) ? $_COOKIE["user"] : "";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>課題L6-4</title>
+</head>
+<body>
+    <form method="post">
+        <label for="name">名前を入力：</label>
+        <input type="text" id="name" name="name" value="<?= $nameCookie ?>">
+        <input type="submit" value="保存">
+    </form>
+    <p><?= $_SESSION['count'] ?>回目の訪問です。</p>
+</body>
+</html>
