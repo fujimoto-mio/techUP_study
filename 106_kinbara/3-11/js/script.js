58d7de37c67ea820f8b415db78096b7a9c3842ed
LF-藤本 <tuyopen_t@yahoo.co.jp>
2023-12-18 08:54:08 +0900

    Revert "Master"

--- 106_kinbara/3-11/js/script.js
+++
@@ -1,9 +0,0 @@
-var arr = ["a","b","c"];
- 
-var result = arr.map(function( value ) {
- 
-    return value + "_set";
- 
-});
- 
-console.log("output:"+ result );
--- 106_kinbara/3-13/js/script.js
+++
@@ -1,5 +0,0 @@
-
-for (let i = 0; i < 1 ; i++){
-  let num = Math.floor(Math.random() * 6)+1;
-  console.log(num);
-}
