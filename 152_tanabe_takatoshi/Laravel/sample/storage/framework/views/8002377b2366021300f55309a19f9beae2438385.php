c443d904f7e257b475cc7864a84d0c93fa6c353b
tanabetakato <t.akato.h14@gmal.com>
2024-02-05 21:30:58 +0900

    P-9 課題提出

---
+++ 152_tanabe_takatoshi/Laravel/sample/storage/framework/views/8002377b2366021300f55309a19f9beae2438385.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>テスト</title>
+</head>
+<body>
+
+  <?php if($todo_lists->isNotEmpty()): ?>
+    <ul>
+      <?php $__currentLoopData = $todo_lists; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <li>
+          <?php echo e($item->name); ?>
+
+        </li>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </ul>
+  <?php endif; ?>
+</body>
+</html><?php /**PATH /Applications/MAMP/htdocs/techUP_study/152_tanabe_takatoshi/Laravel/sample/resources/views/todo_list/index.blade.php ENDPATH**/ ?>
