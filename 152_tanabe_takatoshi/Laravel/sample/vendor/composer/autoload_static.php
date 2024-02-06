06325711d17ebf1baabea6bd9a57dca90fb6fd63
tanabetakato <t.akato.h14@gmal.com>
2024-02-06 22:51:47 +0900

    P-9再提出

--- 152_tanabe_takatoshi/Laravel/sample/vendor/composer/autoload_static.php
+++ 152_tanabe_takatoshi/Laravel/sample/vendor/composer/autoload_static.php
@@ -509,6 +509,7 @@ class ComposerStaticInitc61ff82cbf0142a401a48a8161e1595a
         'App\\Console\\Kernel' => __DIR__ . '/../..' . '/app/Console/Kernel.php',
         'App\\Exceptions\\Handler' => __DIR__ . '/../..' . '/app/Exceptions/Handler.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
+        'App\\Http\\Controllers\\TodoListController' => __DIR__ . '/../..' . '/app/Http/Controllers/TodoListController.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\Authenticate' => __DIR__ . '/../..' . '/app/Http/Middleware/Authenticate.php',
         'App\\Http\\Middleware\\EncryptCookies' => __DIR__ . '/../..' . '/app/Http/Middleware/EncryptCookies.php',
