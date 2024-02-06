c8c3c19b767ce9e2dc87953c0a2a20e44fc7cb41
tanabetakato <t.akato.h14@gmal.com>
2024-02-07 00:22:33 +0900

    競合訂正前に変更

--- 152_tanabe_takatoshi/Laravel/sample/database/seeders/DatabaseSeeder.php
+++ 152_tanabe_takatoshi/Laravel/sample/database/seeders/DatabaseSeeder.php
@@ -13,9 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        //$this->call([
-        //TodoListSeeder::class
-        //]);
+        //\App\Models\User::factory(10)->create();
+        $this->call([
+            TodoListSeeder::class
+        ]);
     }
 }
