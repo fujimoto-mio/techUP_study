2aba2e55c7ba6f9c564f9e1c89b1fd70d7850d4e
t.takato <139970407+takato0523@users.noreply.github.com>
2024-02-17 19:35:36 +0900

    database  修正
    batabaseに追加

--- 152_tanabe_takatoshi/Laravel/sample/config/database.php
+++ 152_tanabe_takatoshi/Laravel/sample/config/database.php
@@ -50,6 +50,9 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
 
             'prefix' => '',
             'prefix_indexes' => true,
