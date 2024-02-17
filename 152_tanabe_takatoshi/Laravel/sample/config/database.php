d0c4855bfdc3cb3f4f5c51271c5d6944357767de
tanabetakato <t.akato.h14@gmal.com>
2024-02-17 20:25:56 +0900

    ファイル修正

--- 152_tanabe_takatoshi/Laravel/sample/config/database.php
+++ 152_tanabe_takatoshi/Laravel/sample/config/database.php
@@ -50,6 +50,10 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
+<<<<<<< HEAD
+=======
+            'password' => env('DB_PASSWORD', ''),
+>>>>>>> fd14229f (ファイル修正)
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
