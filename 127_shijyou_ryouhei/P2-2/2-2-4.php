1bb35bfbc737956197d68e7f8f9ae540ef8c381c
shijyoR <ryohei.shijyo@gmail.com>
2024-03-01 21:10:28 +0900

    課題提出

---
+++ 127_shijyou_ryouhei/P2-2/2-2-4.php
@@ -0,0 +1,31 @@
+<?php
+$bool1 = true;
+$bool2 = false;
+
+$int1 = -1;
+$int2 = 0;
+
+$float = 3.14;
+
+$string1 = '文字列';
+$string2 = '';
+
+$array1 = ['apple', 'banana'];
+
+$array2 = ['apple' => 'リンゴ', 'banana' => 'バナナ'];
+
+$test = null;
+
+printf($bool1);
+printf($bool2);
+
+printf($int1);
+printf($int2);
+
+printf($float);
+
+printf($string1);
+printf($string2);
+
+printf($array1);
+printf($array2);
