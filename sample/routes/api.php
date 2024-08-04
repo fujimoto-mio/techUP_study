28ab7f017fb4a74783b8286795d79f8a02f495ed
Nokngnm01 <nao047463@gmail.com>
2024-08-04 20:28:19 +0900

    課題を提出します

--- sample/routes/api.php
+++ sample/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiSample;
+
+Route::get('/apisample', [ApiSample::class, 'apiHello']);
+Route::post('/nameset', [ApiSample::class, 'nameSet']);
 
 /*
 |--------------------------------------------------------------------------
