e4e4613eabd6baca8ec96763b4c85b62f0c253da
Nokngnm01 <nao047463@gmail.com>
2024-08-22 23:02:16 +0900

    2回目のコミット

---
+++ 178_naganuma_naoki/sample/app/Http/Controllers/ApiSample.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class ApiSample extends Controller
+{
+    public function apiHello()
+    {
+        return response()->json(
+            [
+                'name' => '田中',
+                'nick_name' => 'tanaka',
+                'age' => 26,
+                'profile' => [
+                    'sport' => 'basebool',
+                    'like' => 'move'
+                ]
+            ]
+        );
+        //return 'Hello API!!'; // コメントアウトされた行
+    }
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * ↓これを実行するとレスポンスが返ってきてデータに保存される。
+     * curl -X POST -d "name=tanaka" http://localhost:8000/api/nameset
+     */
+    public function nameSet(Request $request)
+    {
+
+        
+        // 新しいタスクを作成
+        $task = new Task();
+        // タスクのプロパティに値を設定
+        $task->name = $request->input('name');
+        $task->status = 1; // 固定のステータス値
+
+        // データベースに保存
+        $task->save();
+        // 保存したタスクを配列に変換
+        $member = [
+            'name' => $task->name,
+            'status' => $task->status,
+        ];
+       /* JSONに変換してレポンスを返している */
+        return response()->json($member);
+    }
+}
--- 178_naganuma_naoki/sample/routes/api.php
+++ 178_naganuma_naoki/sample/routes/api.php
@@ -1,3 +1,5 @@
+<?php
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiSample;
