4dc859ab0d11b977c5eeb02b32d631f97da76473
yamashita <12choco.chanminto@gmail.com>
2024-09-12 21:34:08 +0900

    p6課題再提出

--- 194_yamashita_haruka/level7_3_table_study.sql
+++ 194_yamashita_haruka/level7_3_table_study.sql
@@ -24,14 +24,14 @@ DROP TABLE IF EXISTS `m_profiles`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `m_profiles` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `name` varchar(100) NOT NULL COMMENT '???O',
-  `kana` varchar(100) NOT NULL COMMENT '?J?i',
-  `age` int(11) NOT NULL COMMENT '?N??',
-  `profile` text NOT NULL COMMENT '?v???t?B?[??',
-  `place` int(5) NOT NULL COMMENT '?o?g?s???{???n?ԍ?',
-  `sex` int(2) NOT NULL COMMENT '????',
-  `created` datetime DEFAULT NULL COMMENT '?o?^??',
-  `modified` datetime DEFAULT NULL COMMENT '?X?V??',
+  `name` varchar(100) NOT NULL COMMENT '名前',
+  `kana` varchar(100) NOT NULL COMMENT 'カナ',
+  `age` int(11) NOT NULL COMMENT '年齢',
+  `profile` text NOT NULL COMMENT 'プロフィール',
+  `place` int(5) NOT NULL COMMENT '出身都道府県地番号',
+  `sex` int(2) NOT NULL COMMENT '性別',
+  `created` datetime DEFAULT NULL COMMENT '登録日',
+  `modified` datetime DEFAULT NULL COMMENT '更新日',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -44,6 +44,33 @@ LOCK TABLES `m_profiles` WRITE;
 /*!40000 ALTER TABLE `m_profiles` DISABLE KEYS */;
 /*!40000 ALTER TABLE `m_profiles` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `m_users`
+--
+
+DROP TABLE IF EXISTS `m_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `m_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `user_name` varchar(100) NOT NULL COMMENT 'ユーザー名',
+  `mail_address` varchar(200) NOT NULL COMMENT 'メールアドレス',
+  `password` varchar(100) NOT NULL COMMENT 'パスワード',
+  `created` datetime DEFAULT NULL COMMENT '登録日',
+  `modified` datetime DEFAULT NULL COMMENT '更新日',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `m_users`
+--
+
+LOCK TABLES `m_users` WRITE;
+/*!40000 ALTER TABLE `m_users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `m_users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -54,4 +81,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-12  8:23:56
+-- Dump completed on 2024-09-12 21:27:34
