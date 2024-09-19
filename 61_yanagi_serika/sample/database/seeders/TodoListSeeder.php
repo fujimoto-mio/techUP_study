9c744f58b1406dcd79249ac383249edf50816494
Serika-Yanagi <serika.11268@gmail.com>
2024-09-19 21:43:25 +0900

    P-9再提出

--- 61_yanagi_serika/sample/database/seeders/TodoListSeeder.php
+++ 61_yanagi_serika/sample/database/seeders/TodoListSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB; 
-
+use Illuminate\Support\Facades\DB; //追記
+ 
 class TodoListSeeder extends Seeder
 {
     /**
