90e3c9c7543023a7a0bced257dead2675534db2e
fujimoto <fujimoto04@gmail.com>
2024-11-08 07:41:35 +0900

    delete

--- 180_kuwahara_yuuhi/p-9/Laravel/Sample/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/list', [\App\Http\Controllers\TodoListController::class, 'index']);
