1a0da0a7f222620b74b2c3315f27d9219cafc42a
Ryosuke Otsuka <163382981+OR1053@users.noreply.github.com>
2025-02-07 10:57:27 +0900

    修正版

--- 156_ootsuka_ryousuke/Laravel/sample/app/Http/Controllers/ApiSample.php
+++ 156_ootsuka_ryousuke/Laravel/sample/app/Http/Controllers/ApiSample.php
@@ -5,8 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Task;
 
+
 class ApiSample extends Controller
 {
+public function apiHello(){
+    return response() -> json(
+        [
+            'name' => '田中',
+            'nick_name' => 'tanaka',
+            "age" => 26,
+            "profile" => ["sport" => 'basebool', "like" => "move"]
+        ]
+    );
+    
+    //return 'Hello API!!';
+}
+
+
+
     //APIが実行される関数
     public function nameSet(Request $request)
     {
