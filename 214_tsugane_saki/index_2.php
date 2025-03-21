24446be958da4305579497cd7456f31245bdb657
tsugane_saki <unagiike@gmail.com>
2025-03-21 23:20:39 +0900

    課題を提出します

---
+++ 214_tsugane_saki/index_2.php
@@ -0,0 +1,13 @@
+<?php
+    $a = 7;
+    $b = 2;
+
+    $kasan = $a + $b;
+    $genzan = $a - $b;
+    $jozan = $a * $b;
+    $josan = $a / $b;
+    
+    echo '加算結果：' . $kasan . '<br>';
+    echo '減算結果：' . $genzan . '<br>';
+    echo '乗算結果：' . $jozan . '<br>';
+    echo '除算結果：' . $josan . '<br>';
