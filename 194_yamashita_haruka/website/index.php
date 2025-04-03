e364aa676a25c138619822e85811f19dcb047012
yamashita <12choco.chanminto@gmail.com>
2025-04-03 21:42:14 +0900

    PHILOSOPHYのｐの幅の調整、PHILOSOPHY・VISIONとSERVICEの説明文の行間を揃える調整

--- 194_yamashita_haruka/website/index.php
+++ 194_yamashita_haruka/website/index.php
@@ -86,7 +86,7 @@ if (isset($_POST["back"]) && $_POST["back"]) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SUN.株式会社</title>
-    <link rel="stylesheet" href="style.css?20250400202">
+    <link rel="stylesheet" href="style.css?202504003">
     <link rel="icon" href="images/favicon.ico">
 </head>
 <body>
