92b55d05e164338eb697ae2de97153fa4e69b60b
ootsuka_ryousuke <o.ryosuke.1053@gmail.com>
2025-07-03 01:40:28 +0900

    P-8提出

---
+++ 156_ootsuka_ryousuke/Laravel/sample/app/Http/Controllers/ApiSample.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+
+class ApiSample extends Controller
+{
+public function apiHello(){
+    return response() -> json(
+        [
+            'name' => '田中',
+            'nick_name' => 'tanaka',
+            "age" => 26,
+            "profile" => ["sport" => 'basebool', "like" => "move"]
+        ]
+    );
+    
+    //return 'Hello API!!';
+}
+
+public function nameSet(Request $request)
+    {
+        
+        $task = new Task;
+
+        $task->name = $request->input('name');
+        $task->status = 1;
+        $task->save();
+
+        $member = [
+            'name' => $task->name,
+            'status' => 1, // statusを固定
+        ];
+
+        return response()->json($member);
+
+    }
+
+
+}
